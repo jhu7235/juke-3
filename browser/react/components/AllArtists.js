a5225fba493a9d772154380c69c4d7cd4ee928dc
Jason <jhu7235@github.com>
2017-07-06 16:02:08 -0500

    mid push

--- browser/react/components/AllArtists.js
+++ browser/react/components/AllArtists.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+
 export default class AllArtists extends Component {
 
   constructor () {
     super();
     this.state = {
-      artists: []
+      artists: [],
+      inputValue: ''
     };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount () {
@@ -17,12 +20,35 @@ export default class AllArtists extends Component {
       .then(artists => this.setState({ artists }));
   }
 
+  handleChange(event) {
+    this.setState( {
+      inputValue: event.target.value
+    });
+    this.matchInputValue();
+  }
+
+  matchInputValue() {
+    let inputValue = this.state.inputValue;
+    if (inputValue) {
+      let filteredArtist = this.state.artists.filter( artist => {
+        return artist.name.match(inputValue);});
+      this.setState( { artists: filteredArtist });
+    }
+  }
+
   render () {
 
-    const artists = this.state.artists;
-
+    let artists = this.state.artists;
+    console.log(artists);
     return (
       <div>
+        <form className="form-group" style={{marginTop: '20px'}}>
+          <input
+            onChange = {this.handleChange}
+            className="form-control"
+            placeholder="Enter artist name"
+          />
+        </form>
         <h3>Artists</h3>
         <div className="list-group">
           {
